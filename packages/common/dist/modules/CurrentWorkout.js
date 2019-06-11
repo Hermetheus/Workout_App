6d2091b0d3a12ed3fd9ba0657fe3a0357e257d3a
Allan Olson <allanmolson@gmail.com>
2019-06-10 21:33:42 -0500

    adding old repo

---
+++ packages/common/dist/modules/CurrentWorkout.js
@@ -0,0 +1,71 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var dayjs_1 = __importDefault(require("dayjs"));
+var mobx_react_lite_1 = require("mobx-react-lite");
+var React = __importStar(require("react"));
+var react_native_1 = require("react-native");
+var rootStore_1 = require("../stores/rootStore");
+var WorkoutCard_1 = require("../ui/WorkoutCard");
+var WorkoutTimer_1 = require("../ui/WorkoutTimer");
+var styles = react_native_1.StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fafafa"
+    },
+    scrollContainer: {
+        padding: 10,
+        marginBottom: 50
+    }
+});
+exports.CurrentWorkout = mobx_react_lite_1.observer(function (_a) {
+    var history = _a.history, _b = _a.match.params, day = _b.day, month = _b.month, year = _b.year;
+    var rootStore = React.useContext(rootStore_1.RootStoreContext);
+    React.useEffect(function () {
+        return function () {
+            rootStore.workoutTimerStore.stopTimer();
+        };
+    }, []);
+    var isCurrentWorkout = !year && !month && !day;
+    var dateKey = year + "-" + month + "-" + day;
+    return (React.createElement(react_native_1.View, { style: styles.container },
+        React.createElement(react_native_1.ScrollView, { keyboardShouldPersistTaps: "always", contentContainerStyle: styles.scrollContainer },
+            (isCurrentWorkout
+                ? rootStore.workoutStore.currentExercises
+                : rootStore.workoutStore.history[dateKey]).map(function (e) {
+                return (React.createElement(WorkoutCard_1.WorkoutCard, { onSetPress: function (setIndex) {
+                        rootStore.workoutTimerStore.startTimer();
+                        var v = e.sets[setIndex];
+                        var newValue;
+                        if (v === "") {
+                            newValue = "" + e.reps;
+                        }
+                        else if (v === "0") {
+                            rootStore.workoutTimerStore.stopTimer();
+                            newValue = "";
+                        }
+                        else {
+                            newValue = "" + (parseInt(v) - 1);
+                        }
+                        e.sets[setIndex] = newValue;
+                    }, key: e.exercise, sets: e.sets, excercise: e.exercise, repsAndWeight: e.numSets + "x" + e.reps + " " + e.weight }));
+            }),
+            React.createElement(react_native_1.Button, { title: "SAVE", onPress: function () {
+                    if (isCurrentWorkout) {
+                        rootStore.workoutStore.history[dayjs_1.default().format("YYYY-MM-DD")] =
+                            rootStore.workoutStore.currentExercises;
+                        rootStore.workoutStore.currentExercises = [];
+                    }
+                    history.push("/");
+                } })),
+        rootStore.workoutTimerStore.isRunning ? (React.createElement(WorkoutTimer_1.WorkoutTimer, { percent: rootStore.workoutTimerStore.percent, currentTime: rootStore.workoutTimerStore.display, onXPress: function () { return rootStore.workoutTimerStore.stopTimer(); } })) : null));
+});
---
+++ packages/common/dist/stores/WorkoutStore.d.ts
@@ -0,0 +1,26 @@
+import { RootStore } from "./rootStore";
+declare type WorkoutDay = "a" | "b";
+export interface CurrentExercise {
+    weight: number;
+    reps: number;
+    numSets: number;
+    exercise: string;
+    sets: string[];
+}
+interface WorkoutHistory {
+    [key: string]: CurrentExercise[];
+}
+export declare class WorkoutStore {
+    rootStore: RootStore;
+    constructor(rootStore: RootStore);
+    currentSquat: number;
+    currentBenchPress: number;
+    currentOverheadPress: number;
+    currentDeadlift: number;
+    currentBarbellRow: number;
+    lastWorkoutType: WorkoutDay;
+    currentExercises: CurrentExercise[];
+    readonly hasCurrentWorkout: boolean;
+    history: WorkoutHistory;
+}
+export {};
